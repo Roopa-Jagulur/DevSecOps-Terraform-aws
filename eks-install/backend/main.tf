9fdcb2feaa17b9b9bb09de8b8f9976600af4bdeb
Roopa-Jagulur <narpala.roopa@gmail.com>
2026-04-29 14:33:02 +0100

    Fixed couple of checkov errors.

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -43,7 +43,24 @@ resource "aws_kms_key" "dynamodb_key" {
   description             = "KMS key for DynamoDB Terraform lock table"
   deletion_window_in_days = 7
   enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "Enable IAM User Permissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
 }
+
+data "aws_caller_identity" "current" {}
 
 resource "aws_dynamodb_table" "terraform_locks" {
   name         = "dso-terraform-eks-state-locks"
